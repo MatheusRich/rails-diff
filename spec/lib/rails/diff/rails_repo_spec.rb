213663528319728fe6c489b583e1cec04d96cef0
Matheus Richard <matheusrichardt@gmail.com>
2025-06-13 23:04:23 -0300

    Make specs faster

--- spec/lib/rails/diff/rails_repo_spec.rb
+++ spec/lib/rails/diff/rails_repo_spec.rb
@@ -1,7 +1,7 @@
 # Helper indices for commits:
-#   git_repo.commits[0] => 'add railties dir' (initial commit)
-#   git_repo.commits[1] => 'commit1'
-#   git_repo.commits.last => 'commit2'
+#   @git_repo.commits[0] => 'add railties dir' (initial commit)
+#   @git_repo.commits[1] => 'commit1'
+#   @git_repo.commits.last => 'commit2'
 
 require "rails/diff/rails_repo"
 require "fileutils"
@@ -11,8 +11,9 @@ RSpec.describe Rails::Diff::RailsRepo do
   let(:logger) { spy }
   let(:cache_dir) { Dir.mktmpdir }
   let(:rails_path) { File.join(cache_dir, "rails") }
-  let(:git_repo) do
-    GitRepo.new.tap do |repo|
+
+  before(:all) do
+    @git_repo = GitRepo.new.tap do |repo|
       repo.add_commit("commit1")
       repo.add_commit("commit2")
     end
@@ -20,12 +21,12 @@ RSpec.describe Rails::Diff::RailsRepo do
 
   after do
     FileUtils.rm_rf(cache_dir)
-    git_repo.cleanup
+    @git_repo.cleanup
   end
 
   describe "#up_to_date?" do
     it "returns false when repo is not cloned yet" do
-      repo = described_class.new(logger:, cache_dir:, rails_repo: git_repo.remote_repo)
+      repo = described_class.new(logger:, cache_dir:, rails_repo: @git_repo.remote_repo)
 
       result = repo.up_to_date?
 
@@ -33,8 +34,8 @@ RSpec.describe Rails::Diff::RailsRepo do
     end
 
     it "returns true when repo is on the latest commit" do
-      git_repo.clone_at_commit(git_repo.commits.last, rails_path)
-      repo = described_class.new(logger:, cache_dir:, rails_repo: git_repo.remote_repo)
+      @git_repo.clone_at_commit(@git_repo.commits.last, rails_path)
+      repo = described_class.new(logger:, cache_dir:, rails_repo: @git_repo.remote_repo)
 
       result = repo.up_to_date?
 
@@ -42,8 +43,8 @@ RSpec.describe Rails::Diff::RailsRepo do
     end
 
     it "returns false and removes the repo when repo is on an old commit" do
-      git_repo.clone_at_commit(git_repo.commits.first, rails_path)
-      repo = described_class.new(logger:, cache_dir:, rails_repo: git_repo.remote_repo)
+      @git_repo.clone_at_commit(@git_repo.commits.first, rails_path)
+      repo = described_class.new(logger:, cache_dir:, rails_repo: @git_repo.remote_repo)
 
       result = repo.up_to_date?
 
@@ -54,16 +55,16 @@ RSpec.describe Rails::Diff::RailsRepo do
 
   describe "#latest_commit" do
     it "returns the latest commit SHA from the remote repo" do
-      repo = described_class.new(logger:, cache_dir:, rails_repo: git_repo.remote_repo)
+      repo = described_class.new(logger:, cache_dir:, rails_repo: @git_repo.remote_repo)
 
       latest = repo.latest_commit
 
-      expect(latest).to eq git_repo.commits.last
+      expect(latest).to eq @git_repo.commits.last
     end
 
     it "returns the new latest commit after a new commit is pushed" do
-      new_commit = git_repo.add_commit("commit3")
-      repo = described_class.new(logger:, cache_dir:, rails_repo: git_repo.remote_repo)
+      new_commit = @git_repo.add_commit("commit3")
+      repo = described_class.new(logger:, cache_dir:, rails_repo: @git_repo.remote_repo)
 
       latest = repo.latest_commit
 
@@ -73,32 +74,32 @@ RSpec.describe Rails::Diff::RailsRepo do
 
   describe "#checkout" do
     it "checks out the given commit in the repo" do
-      git_repo.clone_at_commit(git_repo.commits.last, rails_path)
-      repo = described_class.new(logger:, cache_dir:, rails_repo: git_repo.remote_repo)
+      @git_repo.clone_at_commit(@git_repo.commits.last, rails_path)
+      repo = described_class.new(logger:, cache_dir:, rails_repo: @git_repo.remote_repo)
 
       # Checkout the first file commit (not the initial railties commit)
-      repo.checkout(git_repo.commits[1])
+      repo.checkout(@git_repo.commits[1])
 
       # Verify HEAD is now at the first file commit
       current = Dir.chdir(rails_path) { `git rev-parse HEAD`.strip }
-      expect(current).to eq git_repo.commits[1]
+      expect(current).to eq @git_repo.commits[1]
     end
 
     it "logs the checkout info message" do
-      git_repo.clone_at_commit(git_repo.commits.last, rails_path)
-      repo = described_class.new(logger:, cache_dir:, rails_repo: git_repo.remote_repo)
+      @git_repo.clone_at_commit(@git_repo.commits.last, rails_path)
+      repo = described_class.new(logger:, cache_dir:, rails_repo: @git_repo.remote_repo)
 
-      repo.checkout(git_repo.commits[1])
+      repo.checkout(@git_repo.commits[1])
 
       expect(logger).to have_received(:info)
-        .with(/Checking out Rails \(at commit #{git_repo.commits[1][0..6]}\)/)
+        .with(/Checking out Rails \(at commit #{@git_repo.commits[1][0..6]}\)/)
     end
   end
 
   describe "#install_dependencies" do
     it "runs bundle check and bundle install if needed, and logs appropriately" do
-      git_repo.clone_at_commit(git_repo.commits[0], rails_path)
-      repo = described_class.new(logger:, cache_dir:, rails_repo: git_repo.remote_repo)
+      @git_repo.clone_at_commit(@git_repo.commits[0], rails_path)
+      repo = described_class.new(logger:, cache_dir:, rails_repo: @git_repo.remote_repo)
 
       # Simulate bundle check failing, so bundle install is needed
       allow(Rails::Diff).to receive(:system!).with("bundle check", abort: false, logger: logger).and_return(false)
@@ -111,8 +112,8 @@ RSpec.describe Rails::Diff::RailsRepo do
     end
 
     it "does not run bundle install if bundle check passes" do
-      git_repo.clone_at_commit(git_repo.commits[0], rails_path)
-      repo = described_class.new(logger:, cache_dir:, rails_repo: git_repo.remote_repo)
+      @git_repo.clone_at_commit(@git_repo.commits[0], rails_path)
+      repo = described_class.new(logger:, cache_dir:, rails_repo: @git_repo.remote_repo)
 
       # Simulate bundle check passing
       allow(Rails::Diff).to receive(:system!).with("bundle check", abort: false, logger: logger).and_return(true)
@@ -125,8 +126,8 @@ RSpec.describe Rails::Diff::RailsRepo do
 
   describe "#new_app" do
     it "runs the rails new command with the correct arguments and logs the command" do
-      git_repo.clone_at_commit(git_repo.commits.last, rails_path)
-      repo = described_class.new(logger:, cache_dir:, rails_repo: git_repo.remote_repo)
+      @git_repo.clone_at_commit(@git_repo.commits.last, rails_path)
+      repo = described_class.new(logger:, cache_dir:, rails_repo: @git_repo.remote_repo)
 
       allow(Rails::Diff).to receive(:system!).and_return(true)
       app_name = "myapp"
